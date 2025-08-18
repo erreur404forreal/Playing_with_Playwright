13e3da22ad710fa345f4a0f994788db53327db19
erreur404forreal <ceremej@gmail.com>
2025-08-18 14:35:09 +0200

    ✅ Test — record at cursor tested with github login page

---
+++ tests/recordatcursor-login-github.spec.ts
@@ -0,0 +1,13 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://github.com/');
+  await page.getByRole('link', { name: 'Sign in' }).click();
+  await page.getByRole('textbox', { name: 'Username or email address' }).fill('Testusername');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('false_password');
+  await expect(page.getByRole('button', { name: 'Sign in', exact: true })).toBeVisible();
+  await expect(page.locator('#forgot-password')).toContainText('Forgot password?');
+  await page.getByRole('button', { name: 'Sign in', exact: true }).click();
+  await expect(page.getByRole('alert')).toContainText('Incorrect username or password.');
+});
