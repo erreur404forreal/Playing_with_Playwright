74b446d10250b04c0fb5bd5587a6080f72778ed9
erreur404forreal <ceremej@gmail.com>
2025-08-19 18:19:54 +0200

    🔧 Chore — cleaning comments

--- tests/recordtest-login-github.spec.ts
+++ tests/recordtest-login-github.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://github.com/');
   await page.getByRole('link', { name: 'Sign in' }).click();
-  await page.getByRole('textbox', { name: 'Username or email address' }).fill('Test_username');
+  await page.getByRole('textbox', { name: 'Username or email address' }).fill('Testusername');
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('false_password');
   await page.getByRole('button', { name: 'Sign in', exact: true }).click();
